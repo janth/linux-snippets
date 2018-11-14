f3de74dc77ee2c21ed41c2cb452c6c03afe2f7e4
Jan Thomas Moldung <jan.thomas.moldung@evry.com>
2018-11-14 08:38:55 +0000

    added geoloc

--- getmyip.sh
+++ getmyip.sh
@@ -11,4 +11,15 @@ curl ifconfig.co
 curl ifconfig.me
 curl icanhazip.com
 
+curl --disable ipinfo.io/json
+{
+  "ip": "86.183.80.65",
+  "hostname": "host86-183-80-65.range86-183.btcentralplus.com",
+  "city": "Croydon",
+  "region": "England",
+  "country": "GB",
+  "loc": "51.3776,-0.0496",
+  "postal": "CR0",
+  "org": "AS2856 British Telecommunications PLC"
+
 X
